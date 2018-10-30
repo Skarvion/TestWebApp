766eb6c528af3e2c563e0148385df8bb4d9faf78
Skarvion <williams.saputra@gmail.com>
2018-10-31 01:36:15 +1100

    first

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Will's Test Website</title>
+    <style>
+        h1, p {
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome, William</h1>
+    <p>This is a test website</p>
+    <p>Created by William Saputra</p>
+    <img src="php-1.svg" alt="" width="200">
+</body>
+</html>
